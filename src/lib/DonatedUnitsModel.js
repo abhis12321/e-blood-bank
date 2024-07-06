97e4fe3355ddc016e99f71e886ee448f6e5e691e
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-06 09:28:28 +0530

    DonatedUnitModel is created for saving record of donated blood units

---
+++ src/lib/DonatedUnitsModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const donatedUnitsSchema = new mongoose.Schema({
+    donorRoleId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:[true , "Invalid request, Try again."],
+    },
+    donatedUnits:{
+        type:Number,
+        required:[true , "number of units to be donated is missing."],
+    },
+    donatedTo:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:[true , "where of to whom the units were donated is missings."]
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+});
+
+
+export const donatedUnit = mongoose.models.DonatedUnit || mongoose.model("DonatedUnit" , donatedUnitsSchema);
